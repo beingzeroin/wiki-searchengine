fab2e236f9c0307a129f6fd99a1310c38178ce83
Anish Shankar <rndanish@gmail.com>
2013-01-15 23:34:20 +0530

    Testing xml parsing

--- Makefile
+++ Makefile
@@ -1,6 +1,10 @@
 CCFLAGS = -g -Wall
+all : index
 index : index.o
 	g++ $(CCFLAGS) index.o -o index
 
 index.o : index.cpp
 	g++ -c $(CCFLAGS) index.cpp
+
+clean:
+	rm -rf index *.o
--- index.cpp
+++ index.cpp
@@ -3,11 +3,29 @@
 #include<cstdio>
 #include "rapidxml/rapidxml.hpp"
 using namespace std;
-
+using namespace rapidxml;
+void pc(int c) {
+   while(c--)
+      printf("*");
+}
+void dfs(xml_node<char> *n,int c = 1) {
+   pc(c);
+   printf("%s\n",n->name());
+   if(n->first_node()==0) {
+      pc(c+1);
+      printf("#%s\n",n->value());
+   }
+   n = n->first_node();
+   while(n) {
+      dfs(n,c+1);
+      n=n->next_sibling();
+   }
+}
 int main()
 {
    char xml[] = "<test><sdfs>asdasd</sdfs><asf>afgrgs</asf></test>";
-   rapidxml::xml_document<char> doc;
+   xml_document<char> doc;
    doc.parse<0>(xml);
+   dfs(doc.first_node());
    return 0;
 }
