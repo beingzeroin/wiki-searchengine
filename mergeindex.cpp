91207554d7eb8701a58bd100ed11d2e098bc3090
Anish Shankar <rndanish@gmail.com>
2013-01-18 23:42:18 +0530

    Statistics reporting added

--- mergeindex.cpp
+++ mergeindex.cpp
@@ -4,6 +4,7 @@
 #include <cstdio>
 #include <cstring>
 #include <cassert>
+#include <ctime>
 #include <string>
 #include <vector>
 #include <queue>
@@ -12,15 +13,18 @@ using namespace std;
 FILE *f;
 FILE **tempf;
 vector<vector<pair<int,int> > > current_element;
+long inread = 0;
 //current_element[i][j] is the (docid,freq) pair for the jth document of the current word in file i
 string read_element(int i) {
    current_element[i].clear();
    char buf[50];
    assert(strlen(buf)<=22);
+   inread+=strlen(buf)+1;
    if(fscanf(tempf[i],"%s%*c",buf)==EOF)
       return "";
    int c;
    assert(fread(&c,sizeof(int),1,tempf[i]));
+   inread+=c*4*2+4;
    while(c--) {
       int doc,freq;
       assert(fread(&doc,sizeof(int),1,tempf[i]));
@@ -53,8 +57,25 @@ int main() {
       string s = read_element(i);
       pq.push(make_pair(s,i));
    }
+   int wcnt = 0;
+   long wlsum = 0;
+   clock_t ltime = clock();
+   clock_t start_time = ltime;
+   long lread = 0;
    while(!pq.empty()) {
       string s = pq.top().first;
+      wcnt++;
+      wlsum += s.size();
+      if(wcnt%100000==0){
+	 clock_t etime = clock();
+	 float rate = float(inread-lread)/(1024*1024);
+	 rate /= float(etime-ltime)/CLOCKS_PER_SEC;
+	 ltime = etime;
+	 lread = inread;
+	 int dur = (etime-start_time)/CLOCKS_PER_SEC;
+	 fprintf(stderr,"%d words output, %lu dict size\n",wcnt,wlsum);
+	 fprintf(stderr,"%02d:%02d elapsed, %f read , [%.2f MB/s]\n",dur/60,dur%60,float(inread)/(1024*1024),rate);
+      }
       vector<pair<int,int> > cur_index;
       while(!pq.empty() && pq.top().first==s) {
 	 int id = pq.top().second;
