1b13ddebe0b35624f28d6e9af0ffe70655589bbb
Anish Shankar <rndanish@gmail.com>
2013-02-07 17:32:24 +0530

    Added stripping search term

--- read_index.cpp
+++ read_index.cpp
@@ -6,13 +6,13 @@
 #include <string>
 #include <algorithm>
 #include "varbyteencoder.h"
+#include "tokenize.h"
 using varbyteencoder::decode;
 using namespace std;
 char buf[100];
 vector<pair<string,off_t> > dict;
 
 int main(int argc, char**argv) {
-   fprintf(stderr,"Warning this tool is part of the querying engine, This tool is under construction and has several bugs. Do not Evaluate\n");
    if(argc!=2) {
       fprintf(stderr, "Usage : %s index_file_name\n",argv[0]);
       return 2;
@@ -54,28 +54,43 @@ int main(int argc, char**argv) {
       }
    }
    fprintf(stderr,"\r");
-   printf("%ld Words in Dictionary!",dict.size());
+   printf("%ld Words in Dictionary!\n",dict.size());
+
+
+
    printf("Enter words to query :\n");
    while(scanf("%s",buf)!=EOF) {
-      pair<string,off_t> p;
-      p.first = buf;
-      p.second = 0;
-      p = *lower_bound(dict.begin(),dict.end(),p);
-      printf("Found string : %s\n",p.first.c_str());
-      printf("Offset : %ld\n",p.second);
-      fseeko(f,p.second,SEEK_SET);
-      vector<pair<int,int> > v;
-      fscanf(f,"%*s%*c");
-      decode(f,v);
-      printf("%lu Documents with word\n",v.size());
-      for(auto it = v.begin(); it != v.end(); it++)
-	 swap(it->first, it->second);
-      sort(v.begin(), v.end());
-      int c= 0;
-      for(auto it = v.rbegin(); it != v.rend(); it++) {
-	 c++;
-	 printf("freq:%d doc: %d (%d)\n",it->first,it->second/8, (it->second)%8);
-	 if(c>=15)break;
+      char *red_token = reduce_token(buf);
+      bool valid_word=false;
+      if(red_token != 0) 
+      {
+	 pair<string,off_t> p;
+	 p.first = red_token;
+	 p.second = 0;
+	 auto pt = lower_bound(dict.begin(),dict.end(),p);
+	 if(pt != dict.end() && p.first == pt->first) {
+	    valid_word = true;
+	    p = *pt;
+	    printf("Found string : %s\n",p.first.c_str());
+	    printf("Offset : %ld\n",p.second);
+	    fseeko(f,p.second,SEEK_SET);
+	    vector<pair<int,int> > v;
+	    fscanf(f,"%*s%*c");
+	    decode(f,v);
+	    printf("%lu Documents with word\n",v.size());
+	    for(auto it = v.begin(); it != v.end(); it++)
+	       swap(it->first, it->second);
+	    sort(v.begin(), v.end());
+	    int c= 0;
+	    for(auto it = v.rbegin(); it != v.rend(); it++) {
+	       c++;
+	       printf("freq:%d doc: %d (%d)\n",it->first,it->second/8, (it->second)%8);
+	       if(c>=15)break;
+	    }
+	 }
+      }
+      if(!valid_word) {
+	 printf("Word discarded...\n");
       }
 
    }
