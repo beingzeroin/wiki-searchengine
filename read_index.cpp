54cfe35747a497ad5e350b8b66ae27b24587a7b3
Anish Shankar <rndanish@gmail.com>
2013-02-11 18:31:03 +0530

    Reset min word count restriction as severly affects accuracy in title queries

--- read_index.cpp
+++ read_index.cpp
@@ -20,7 +20,7 @@ using namespace std;
 #define TITLE(x) ((x&3)==2)
 #define CATEG(x) ((x&3)==3)
 
-#define MIN_WORD_COUNT 50 // To prevent short document normalization issues
+#define MIN_WORD_COUNT 0 // To prevent short document normalization issues
 
 char buf[100];
 vector<pair<string,off_t> > dict;
@@ -36,7 +36,7 @@ int get_doc_wc(int doc_id) {
    pair<int,pair<int,string> > p;
    p.first = doc_id;
    auto it = lower_bound(document_list.begin(),document_list.end(), p);
-   return max(50,it->second.first);
+   return max(MIN_WORD_COUNT,it->second.first);
 }
 string get_doc_title(int doc_id) {
    pair<int,pair<int,string> > p;
